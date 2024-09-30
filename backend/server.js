0cc04b8459fce6112fb145567ce87eb2d3da4e58
ELABBASSI Oussama <41241807+alpaci7@users.noreply.github.com>
2024-09-30 10:12:43 +0100

    Update server.js plc esp

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 
 app.get('/',(req, res)=>{
-  res.status(200).json({message : "hello"});
+  res.status(200).json({message : "hello ESP"});
 });
 // Route to control the ESP32 LED
 app.post('/control-led', async (req, res) => {
